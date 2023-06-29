7d45bb5b8ae9fbd9cfa27d50f20199833d632344
kelwinv <57596293+kelwinv@users.noreply.github.com>
2023-06-29 17:57:51 -0300

    Delete years-of-experience.js

--- years-of-experience.js
+++
@@ -1,6 +0,0 @@
-const startDate = new Date('2019-01-01'); // Substitua com a sua data de início
-const currentDate = new Date();
-const diffTime = Math.abs(currentDate - startDate);
-const diffYears = Math.floor(diffTime / (1000 * 60 * 60 * 24 * 365));
-
-document.getElementById('years-of-experience').textContent = diffYears;
